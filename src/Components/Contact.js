3f3748b09e9480fe40086d9726b0b15ee08c9da9
wwhite13sub <64095383+wwhite13sub@users.noreply.github.com>
2020-09-04 14:46:37 -0500

    Reviewing of code

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,3 +1,128 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+import { connect } from 'react-redux';
+import { addContact } from './../redux/actions/contact';
+import { getContacts } from './../redux/selectors/contact';
+import  {changeActive } from './../redux/actions/navbar';
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            comments: '',
+            error: '',
+            success: ''
+        }      
+    }
+
+    showError = () => {
+        if (!this.state.error.length) {
+            return;
+        }
+
+        return (
+            <div className="alert alert-danger mt-3" role="alert">
+                {this.state.error}
+            </div>
+        );
+    }
+
+    showSuccess = () => {
+        if (!this.state.success.length) {
+            return;
+        }
+
+        return (
+            <div className="alert alert-success mt-3" role="alert">
+                {this.state.success}
+            </div>
+        );
+    }
+
+    handleFirstNameChange = (e) => {
+        this.setState({
+            firstName: e.target.value
+        });
+    }
+    handleLastNameChange = (e) => {
+        this.setState({
+            lastName: e.target.value
+        });
+    }
+    handleEmailChange = (e) => {
+        this.setState({
+            email: e.target.value
+        });
+    }
+    handleCommentsChange = (e) => {
+        this.setState({
+            comments: e.target.value
+        });
+    }
+
+    addContact = (e) => {
+        e.preventDefault();
+        
+        //validation
+        if (!this.state.firstName || (this.state.firstName.length == 0)
+            || !this.state.lastName || (this.state.lastName.length == 0)
+            || !this.state.email || (this.state.email.length == 0)
+            || !this.state.comments || (this.state.comments.length == 0)
+        ) {
+            this.setState({
+                error: 'First Name, Last Name, Email and Comments are required.',
+                success: ''
+            });
+            return;
+        }
+
+        //check length
+        if (this.state.firstName.length > 20
+            || this.state.lastName.length > 20
+            || this.state.comments.length > 20
+        ) {
+            this.setState({
+                error: 'All the inputs can be max of 20 char. length.',
+                success: ''
+            });
+            return;
+        }
+
+        //email validation
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(this.state.email.toLowerCase())) {
+            this.setState({
+                error: 'Please enter a valid email.',
+                success: ''
+            });
+            return;
+        }
+
+        this.setState({
+            error: ''
+        });
+        
+        // deleted adding contacts function once submit on contact from, caused too many small errors
+        this.props.history.push("/Todos");
+        this.props.changeActive('todos');
+        this.cleanForm();
+    }
+
+    cleanForm = () => {
+        this.setState({
+            firstName: '',
+            lastName: '',
+            email: '',
+            comments: '',
+            success: 'New Contact has been added.'
+        });
+    }
+
+
 render() {
     return (
         <div id="name" className="row">
@@ -28,3 +153,16 @@ render() {
     )
 }
 }
+
+const mapStateToProps = state => {
+    return {
+        contactsList: getContacts(state.contact.contactsList),
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    addContact: (state) => dispatch(addContact(state)),
+    changeActive: (name) => dispatch(changeActive(name))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Contact);
