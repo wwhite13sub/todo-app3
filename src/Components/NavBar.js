9240ae4cf25820dbeed2f3e330d73ddeda8c1d64
wwhite13sub <64095383+wwhite13sub@users.noreply.github.com>
2020-09-04 05:12:57 -0500

    Adding bootstrap Navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -9,3 +9,26 @@ class NavBar extends React.Component {
           active: 'project' /*Project to be "Home*/
       }
     }
+    
+    render() {
+        return ( 
+          <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+            <img src={Images} className="AppImg2" alt="logo" />
+            <Link onClick={this.changeActiveToProject} className={(this.props.active==='project'?'active bar':'bar')} to="/">
+              Project
+            </Link>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Link onClick={this.changeActiveToTodo} className={(this.props.active==='todos'?'active bar':'bar')} to="/Todos">
+                    Todo
+                </Link>
+                <Link onClick={this.changeActiveToContact} className={(this.props.active==='contact'?'active bar':'bar')} to="/Contact">
+                    Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar> 
+        );
+      };
+    }
