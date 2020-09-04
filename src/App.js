acb6fd571d25406898461c3d25b30aea5b22afa2
wwhite13sub <64095383+wwhite13sub@users.noreply.github.com>
2020-09-04 05:03:45 -0500

    Adding Needed Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NavBar from './Components/NavBar';
+import Todos from './Components/Todos';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
