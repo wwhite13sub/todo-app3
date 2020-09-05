25572c928cc12c68f97fcc51309073f854aefaa3
wwhite13sub <64095383+wwhite13sub@users.noreply.github.com>
2020-09-04 20:55:11 -0500

    updating home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,3 +1,24 @@
-/* Project Details will not include actual functions "My Design page"*/
 import React, { Component } from 'react'
 import Images from '../Images/TodoImg.jpg'
+
+class Home extends Component {
+
+    render() {
+        return (
+            
+            <div className="container">
+               <img src={Images} className="AppImg" alt="logo" />
+                <div className="chart">
+                <p><strong>Project Name: </strong>Accomplish Todo</p>
+                <p><strong>Project Due Date: </strong>12/20/21</p>
+                <p><strong>Project Objective: </strong>Create Todo List Web Application</p>
+                <p><strong>Project Participants: </strong> D.Pearl,  K.Pearl,  W.White,</p>
+              
+    
+            </div>
+            </div>
+            
+        )
+    }
+}
+export default Home;
