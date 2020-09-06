d6f777c100d6c42b56f9cbe41258a9cc2435b476
wwhite13sub <64095383+wwhite13sub@users.noreply.github.com>
2020-09-05 20:50:38 -0500

    resolving clash

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import NavBar from './Components/NavBar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Todos from './Components/Todos';
 import Home from './Components/Home';
 import Contact from './Components/Contact';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+class App extends Component {
 
-
-
-export default App;
+    render() {
+      return (
+        <Router>
+          <div className="container-fluid">
+            <NavBar />
+          
+            <Route path='/' component={Home} exact/>
+            <Route path='/Contact' component={Contact}/>
+            <Route path='/Todos' component={Todos} />
+          </div>
+        </Router>
+      );
+    }
+  }
+  
+  
+  
+  
+  export default App;
+  
+  
